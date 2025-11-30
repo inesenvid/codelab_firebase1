c9f9433797387a0c701674d37eca66216fbbfa0d
Ines Envid <inese@google.com>
2025-11-29 17:37:08 -0800

    Fix TS2345 error in app.config.ts for Firebase providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,21 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
-import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core'; // Ensure importProvidersFrom is removed here if it was present
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+// Add other Firebase providers here if you need them, e.g., provideFirestore, provideStorage
+
+import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes)
-  ],
+    provideRouter(routes),
+    // Place Firebase providers directly in the array, WITHOUT importProvidersFrom
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    // Add other Firebase providers here, e.g.:
+    // provideFirestore(() => getFirestore()),
+    // provideStorage(() => getStorage()),
+  ]
 };
+
