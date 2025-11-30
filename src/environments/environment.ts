8c4f3ef207e4670c92e3387e7ab78d997b9147de
Ines Envid <inese@google.com>
2025-11-29 19:42:34 -0800

    Fixed environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-apiKey: "AIzaSyAXbvbfGD5UXUeIx8SJPTkSjeXcTVKMALw",
+  apiKey: "AIzaSyAXbvbfGD5UXUeIx8SJPTkSjeXcTVKMALw",
   authDomain: "sage-nucleus-427103-s9-5b85c.firebaseapp.com",
   projectId: "sage-nucleus-427103-s9-5b85c",
   storageBucket: "sage-nucleus-427103-s9-5b85c.firebasestorage.app",
   messagingSenderId: "1043538624599",
-  appId: "1:1043538624599:web:35de92a00e54b6e5196312",
-  measurementId: "G-SLN6J8EDBZ"
+appId: "1:1043538624599:web:5db099896ee30c9f196312",  
+measurementId: "G-SLN6J8EDBZ"
   },
 };
